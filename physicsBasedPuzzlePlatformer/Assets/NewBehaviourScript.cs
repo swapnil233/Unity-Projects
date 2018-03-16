b3aed6b2ac090dfd0245e775f572b6283fbe1a16
Swapnil-Laptop <swapniliqbal@gmail.com>
2018-03-16 16:16:46 -0700

    Initial Commit of Physics Ball Game

---
+++ physicsBasedPuzzlePlatformer/Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    public int multiplier = 0;
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        float Jump = Input.GetAxis("Jump");
+        float movehorizontal = Input.GetAxis("Horizontal");
+        float movevertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(movehorizontal, Jump, movevertical);
+            rb.AddForce(movement * multiplier);
+        
+	}
+}
