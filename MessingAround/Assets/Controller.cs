dc573e1d0d2598468ec07a76496fa2407c28f74d
Swapnil-Laptop <swapniliqbal@gmail.com>
2018-03-17 01:15:04 -0700

    Messing Around with Rigid Body

---
+++ MessingAround/Assets/Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;
+    public float jumpSpeed;
+
+    // Use this for initialization
+    void Start () {
+
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float xSpeed = Input.GetAxis("Horizontal");
+        float ySpeed = Input.GetAxis("Vertical");
+        float jump = Input.GetAxis("Jump");
+
+        Vector3 movement = new Vector3(xSpeed, 0.0f, ySpeed);
+        Vector3 jumpAction = new Vector3(0, jump, ySpeed);
+
+        GetComponent<Rigidbody>().AddForce(movement);
+
+        rb.AddForce(movement * speed * Time.deltaTime);
+        rb.AddForce(jumpAction * jumpSpeed * Time.deltaTime);
+    }
+}
