4a593bd84d35231532a0ff9e1e709d53af2c281b
Swapnil-Laptop <swapniliqbal@gmail.com>
2018-03-18 21:32:03 -0700

    Level 1 version 1.0, and cameraFollow system
    I've made version 1.0 of level one, and the camera follow system. Will keep working on level 1 in coming weeks.

---
+++ physicsBasedPuzzlePlatformer/Assets/Scripts/playerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    public float multiplier;
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {     
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float xSpeed = Input.GetAxis("Horizontal");
+        float ySpeed = Input.GetAxis("Vertical");
+        float jump = Input.GetAxis("Jump");
+
+        Vector3 movement = new Vector3(xSpeed, jump, ySpeed);
+        rb.AddForce(movement * multiplier);
+    }
+}
