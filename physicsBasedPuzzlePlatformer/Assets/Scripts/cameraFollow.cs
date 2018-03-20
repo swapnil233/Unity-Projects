bc289c0cccd6c85838c25279a3e1a458c5bfc79d
Swapnil-Laptop <swapniliqbal@gmail.com>
2018-03-19 18:04:49 -0700

    Level 1 Movement, Jump, and Respawn
    Implemented a jittery movement system using CharaterControl (will change), added jumping system, and Nadir Graeme and I managed to add a respawn system.

--- physicsBasedPuzzlePlatformer/Assets/Scripts/cameraFollow.cs
+++ physicsBasedPuzzlePlatformer/Assets/Scripts/cameraFollow.cs
@@ -6,7 +6,6 @@ public class cameraFollow : MonoBehaviour {
     public Transform target;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
-
     private void FixedUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
@@ -15,8 +14,5 @@ public class cameraFollow : MonoBehaviour {
         transform.position = smoothedPosition;
         
     }
-
-
-
 
 }
--- physicsBasedPuzzlePlatformer/Assets/Scripts/playerController.cs
+++ physicsBasedPuzzlePlatformer/Assets/Scripts/playerController.cs
@@ -4,22 +4,88 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour {
 
-    public float multiplier;
+<<<<<<< HEAD
+    public float speedNum;
+
+    //Character Controller
+    private CharacterController controller;
+    private float verticalVelocity;
+    private float gravity = 10.0f;
+    private float jumpForce = 10.0f;
+
+    //Rigid Body 
     public Rigidbody rb;
+=======
+    public float multiplier;
+    private Rigidbody rb;
+>>>>>>> d4deecbbe3ec1af944e3d916f19f72905dc2dd2c
+
+    //Game object 
+    public GameObject ball;
+
+    //Check for respawn
+    private bool belowground;
+
+
+
 
     // Use this for initialization
-    void Start () {     
+    void Start () {
+
+        belowground = false;
+
+        //Initializing rigid body and char controller @ the start
         rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 	
 	// Update is called once per frame
 	void Update () {
 
+        //Respawn function
+        if (ball.transform.position.y < 0)
+        {
+            belowground = true;
+        }
+
+        if (belowground == true)
+        {
+            ball.transform.position = new Vector3(-2, 5, -8);
+            belowground = false;
+        }
+        
+        /*
+        //VARIABLES
         float xSpeed = Input.GetAxis("Horizontal");
         float ySpeed = Input.GetAxis("Vertical");
         float jump = Input.GetAxis("Jump");
 
-        Vector3 movement = new Vector3(xSpeed, jump, ySpeed);
-        rb.AddForce(movement * multiplier);
+        //Ball Movement
+        Vector3 movement = new Vector3(xSpeed, 0, ySpeed);
+        rb.AddForce(movement * speedNum);
+        */
+
+        //Alternate moving script
+        if (controller.isGrounded)
+        {
+        verticalVelocity = -gravity * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            verticalVelocity = jumpForce;
+        }
+        } else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        //Moving and jumping 
+        Vector3 moveVector = Vector3.zero;
+
+        moveVector.x = Input.GetAxis("Horizontal") * 5.0f;
+        moveVector.y = verticalVelocity;
+        moveVector.z = Input.GetAxis("Vertical") * 5.0f;
+        controller.Move(moveVector * Time.deltaTime);
+
     }
 }
