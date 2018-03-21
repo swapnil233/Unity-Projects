0d1603a57efaa1edbe69b7bc3ba442d0275a6cd9
Couls <coul422@gmail.com>
2018-03-21 19:16:53 -0400

    Some Programming Work
    -Created a new respawn script asset(rather than sticking the code in our controller)
    -Got the camera rotation to move(using something called euler angles and quaternions)
    -removed monkeyball from level 2(FOR NOW)
    -some other stuff i prob forgot about while i was working on this

--- physicsBasedPuzzlePlatformer/Assets/Scripts/cameraFollow.cs
+++ physicsBasedPuzzlePlatformer/Assets/Scripts/cameraFollow.cs
@@ -6,9 +6,9 @@ public class cameraFollow : MonoBehaviour {
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
     public Vector3 offset2;
-
+    private Vector3 up = Vector3.up;
     public GameObject cam;
-
+    public bool camchange;
     public GameObject ball;
 
     private void FixedUpdate()
@@ -17,16 +17,15 @@ public class cameraFollow : MonoBehaviour {
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
 
         transform.position = smoothedPosition;
-
-        if (ball.transform.position.z >= 0)
+            
+        if (ball.transform.position.z > 0 && camchange == true)
         {
 
             Vector3 desiredPosition2 = target.position + offset2;
             Vector3 smoothedPosition2 = Vector3.Lerp(transform.position, desiredPosition2, smoothSpeed);
-            Vector3 smoothedRotation2 = new Vector3(30, 117, 1);
-
+            Quaternion smoothedRotation2 = Quaternion.Euler(30, 117, 1);
             transform.position = smoothedPosition2;
-            transform.Rotate(smoothedRotation2);
+            transform.rotation = smoothedRotation2;
         }
         
     }
